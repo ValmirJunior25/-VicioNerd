0fff480ac56f77d8441a1e689dcb79a278aa0e74
Valmir Júnior <valmirjunior1025@gmail.com>
2026-05-28 19:04:20 -0300

    feat: adiciona model Usuario

---
+++ Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿namespace VicioNerd.Models
+{
+    internal class Usuario
+    {
+        private int id;
+        private string nome;
+        private string email;
+        private string senha;
+
+        public int Id { get; } = 0;
+
+        public string Nome { get; set; } = "";
+
+        public string Email { get; set; } = "";
+
+        public string Senha { get; set; } = "";
+    }
+}
